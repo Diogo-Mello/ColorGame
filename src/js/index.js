d345394b4e22b14849d3fef448edd61f428a60bd
Diogo-Mello <diogomellodacrus1108@gmail.com>
2022-10-18 23:52:23 -0300

    Att 3.0

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,30 @@ const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const color3 = document.getElementById('color3');
 const color4 = document.getElementById('color4');
+var ordem;
+var posicao = 0;
 
-const som = document.getElementById('audio');
+const audioSelecao = document.getElementById('audioSelecao');
+const audioPerdeu = document.getElementById('audioPerdeu');
+const audioVenceu = document.getElementById('audioVenceu');
+
+const indexNivel = document.getElementById('nivel');
+const indexRecord = document.getElementById('record');
+const indexButton = document.getElementById('button');
 
 const tempoEntreElementos = 1000;
 
 let delay = 500;
+var record = 0;
+var nivel;
 
 var numerosOrdenados = [];
 var quantidade = 0;
+
+if (localStorage.record) {
+    record = JSON.parse(localStorage.getItem('record'))
+    indexRecord.innerHTML = record;
+}
 
 function mudarQuantidade() {
     quantidade += 1;
@@ -25,6 +40,10 @@ function gerarNumeros() {
 }
 
 function acenderTudo() {
+    posicao = 0
+
+    removerOnClick();
+
     mudarQuantidade();
 
     gerarNumeros();
@@ -32,53 +51,53 @@ function acenderTudo() {
     acender();
 }
 
-function girarCores() {
+function errou() {
+    posicao = 0;
+    quantidade = 0;
+
+    audioPerdeu.play();
+
     acenderCor1();
+    acenderCor2();
+    acenderCor3();
+    acenderCor4();
 
-    setTimeout(function(){
-        acenderCor2()
-    },300)
+    setTimeout(function () {
+        acenderTudo();
+    }, 1000)
 
-    setTimeout(function(){
-        acenderCor4()
-    },600)
-
-    setTimeout(function(){
-        acenderCor3()
-    },900)
-
-    setTimeout(function(){
-        acenderCor1();
-    },1200)
-
-    setTimeout(function(){
-        acenderCor2()
-    },1500)
-
-    setTimeout(function(){
-        acenderCor4()
-    },1800)
-
-    setTimeout(function(){
-        acenderCor3()
-    },2100)
 }
 
 function acender() {
-    numerosOrdenados.forEach(function (numerosOrdenados, x) {
+    nivel = numerosOrdenados.length;
+    if (nivel > record) {
+        record = nivel;
+        localStorage.record = JSON.stringify(record);
+    }
+    indexNivel.innerHTML = nivel;
+    indexRecord.innerHTML = record;
+
+    numerosOrdenados.forEach(function (numeroOrdenado, x) {
         setTimeout(function () {
-            if (numerosOrdenados == 0) {
+            if (numeroOrdenado == 0) {
                 acenderCor1();
             }
-            else if (numerosOrdenados == 1) {
+            else if (numeroOrdenado == 1) {
                 acenderCor2();
             }
-            else if (numerosOrdenados == 2) {
+            else if (numeroOrdenado == 2) {
                 acenderCor3();
             }
             else {
                 acenderCor4();
             }
+            let ultimaPosicao = numerosOrdenados.length - 1;
+            if (x == ultimaPosicao) {
+                setTimeout(function () {
+                    adicionarOnClick();
+                }, 1000)
+            }
+            audioSelecao.play();
         }, delay)
 
         delay += tempoEntreElementos;
@@ -124,4 +143,69 @@ function acenderCor4() {
         color4.style.backgroundColor = 'var(--cor4)'
         color4.style.boxShadow = ''
     }, 500)
+}
+
+function adicionarOnClick() {
+    color1.setAttribute('onclick', 'verificarAcerto(0)');
+    color2.setAttribute('onclick', 'verificarAcerto(1)');
+    color3.setAttribute('onclick', 'verificarAcerto(2)');
+    color4.setAttribute('onclick', 'verificarAcerto(3)');
+    color1.style.cursor = 'pointer';
+    color2.style.cursor = 'pointer';
+    color3.style.cursor = 'pointer';
+    color4.style.cursor = 'pointer';
+}
+
+function removerOnClick() {
+    color1.removeAttribute('onclick');
+    color2.removeAttribute('onclick');
+    color3.removeAttribute('onclick');
+    color4.removeAttribute('onclick');
+    color1.style.cursor = '';
+    color2.style.cursor = '';
+    color3.style.cursor = '';
+    color4.style.cursor = '';
+}
+
+function verificarAcerto(cor) {
+
+    ordem = numerosOrdenados[posicao]
+
+    if (cor != ordem) {
+        errou();
+    } else if (cor == 0) {
+        audioSelecao.play();
+        acenderCor1();
+        verificarUltimoAcerto()
+        posicao++
+    } else if (cor == 1) {
+        audioSelecao.play();
+        acenderCor2();
+        verificarUltimoAcerto()
+        posicao++
+    } else if (cor == 2) {
+        audioSelecao.play();
+        acenderCor3();
+        verificarUltimoAcerto()
+        posicao++
+    } else {
+        audioSelecao.play();
+        acenderCor4();
+        verificarUltimoAcerto()
+        posicao++
+    }
+}
+
+function verificarUltimoAcerto() {
+    if (posicao == numerosOrdenados.length - 1) {
+        audioVenceu.play()
+        setTimeout(function () {
+            acenderTudo();
+        }, 1000)
+    }
+}
+
+function jogar () {
+    acenderTudo();
+    indexButton.innerHTML = ''
 }
